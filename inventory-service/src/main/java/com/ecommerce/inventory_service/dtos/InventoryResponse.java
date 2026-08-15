66bafa96470dc5ca4923698db9762d2c7a2c41e5
Brayan Landa <landakkpp@gmail.com>
2026-08-15 00:07:43 -0500

    feat: add InventoryRequest and InventoryResponse DTOs, and Inventory model with validation

---
+++ inventory-service/src/main/java/com/ecommerce/inventory_service/dtos/InventoryResponse.java
@@ -0,0 +1,17 @@
+package com.ecommerce.inventory_service.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class InventoryResponse {
+    private Long id;
+    private String sku;
+    private Integer quantity;
+    private boolean isStock;
+}
