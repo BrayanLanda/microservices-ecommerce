074ee16c4b9d5726d24004a056f7a6d5db9a9d50
Brayan Landa <landakkpp@gmail.com>
2026-08-22 09:41:28 -0500

    feat: implement CRUD operations and stock validation logic in InventoryService

--- inventory-service/src/main/java/com/ecommerce/inventory_service/services/InventoryService.java
+++ inventory-service/src/main/java/com/ecommerce/inventory_service/services/InventoryService.java
@@ -4,13 +4,12 @@ import java.util.List;
 
 import com.ecommerce.inventory_service.dtos.InventoryRequest;
 import com.ecommerce.inventory_service.dtos.InventoryResponse;
-import com.ecommerce.inventory_service.repositories.InventoryRepository;
 
 public interface InventoryService {
 
     boolean isInStock(String sku, Integer quantity);
 
-    InventoryRepository createInventory(InventoryRequest inventoryRequest);
+    InventoryResponse createInventory(InventoryRequest inventoryRequest);
 
     List<InventoryResponse> getAllinventory();
 
--- inventory-service/src/main/java/com/ecommerce/inventory_service/services/impl/InventoryServiceImpl.java
+++ inventory-service/src/main/java/com/ecommerce/inventory_service/services/impl/InventoryServiceImpl.java
@@ -7,7 +7,9 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.ecommerce.inventory_service.dtos.InventoryRequest;
 import com.ecommerce.inventory_service.dtos.InventoryResponse;
+import com.ecommerce.inventory_service.exceptions.ResourceNotFoundException;
 import com.ecommerce.inventory_service.mappers.InventoryMapper;
+import com.ecommerce.inventory_service.models.Inventory;
 import com.ecommerce.inventory_service.repositories.InventoryRepository;
 import com.ecommerce.inventory_service.services.InventoryService;
 
@@ -24,31 +26,57 @@ public class InventoryServiceImpl implements InventoryService {
     @Override
     @Transactional(readOnly = true)
     public boolean isInStock(String sku, Integer quantity) {
-
+        return inventoryRepository.findBySku(sku)
+                .map(inventory -> inventory.getQuantity() >= quantity)
+                .orElse(false);
     }
 
     @Override
-    public InventoryRepository createInventory(InventoryRequest inventoryRequest) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'createInventory'");
+    @Transactional
+    public InventoryResponse createInventory(InventoryRequest inventoryRequest) {
+        boolean exists = inventoryRepository.existsBySku(inventoryRequest.getSku());
+        if (exists) {
+            throw new IllegalArgumentException("Inventory already exists");
+        }
+        Inventory inventory = inventoryMapper.toModel(inventoryRequest);
+        Inventory savedInventory = inventoryRepository.save(inventory);
+
+        log.info("Inventory created: {}", savedInventory);
+
+        return inventoryMapper.toResponse(savedInventory);
     }
 
     @Override
+    @Transactional(readOnly = true)
     public List<InventoryResponse> getAllinventory() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getAllinventory'");
+        return inventoryRepository.findAll().stream()
+                .map(inventoryMapper::toResponse)
+                .toList();
     }
 
     @Override
+    @Transactional
     public InventoryResponse updateInventory(Long id, InventoryRequest inventoryRequest) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'updateInventory'");
+        Inventory inventory = inventoryRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Inventoy", "id", id));
+
+        inventory.setSku(inventoryRequest.getSku());
+        inventory.setQuantity(inventoryRequest.getQuantity());
+
+        Inventory updatedInventory = inventoryRepository.save(inventory);
+        log.info("Inventory updated: {}", updatedInventory);
+
+        return inventoryMapper.toResponse(updatedInventory);
     }
 
     @Override
+    @Transactional
     public void deleteInventory(Long id) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'deleteInventory'");
+        if (!inventoryRepository.existsById(id)) {
+            throw new ResourceNotFoundException("Inventoy", "id", id);
+        }
+        inventoryRepository.deleteById(id);
+        log.info("Inventory deleted: {}", id);
     }
 
 }
